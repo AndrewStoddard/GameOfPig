c569c48eb022fde6ec25fd6d7eb92f8be8a6c30b
AndrewStoddard <acs8182000@gmail.com>
2021-02-24 17:35:58 -0500

    Added GetBoolean extension

---
+++ AndrewStoddardGameOfPig/Models/SessionExtensions.cs
@@ -0,0 +1,44 @@
+﻿// ***********************************************************************
+// Author           : Andrew Stoddard
+// Created          : 02-24-2021
+//
+// Last Modified By : Andrew Stoddard
+// Last Modified On : 02-24-2021
+// ***********************************************************************
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndrewStoddardGameOfPig.Models
+{
+    /// <summary>
+    /// Class SessionExtensions.
+    /// </summary>
+    public static class SessionExtensions
+    {
+        /// <summary>
+        /// Sets the boolean.
+        /// </summary>
+        /// <param name="session">The session.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        public static void SetBoolean(this ISession session, string key, bool value)
+        {
+            session.SetString(key, value.ToString());
+        }
+        /// <summary>
+        /// Gets the boolean.
+        /// </summary>
+        /// <param name="session">The session.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>Boolean.</returns>
+        public static Boolean GetBoolean(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+            return Boolean.Parse(value);
+        }
+
+    }
+}
