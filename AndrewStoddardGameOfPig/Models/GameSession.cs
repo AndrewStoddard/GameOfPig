45df12d79cd313bfc321e3f7b01d3ebdf5cb2f8f
AndrewStoddard <acs8182000@gmail.com>
2021-02-24 16:38:38 -0500

    Added extra game session value

--- AndrewStoddardGameOfPig/Models/GameSession.cs
+++ AndrewStoddardGameOfPig/Models/GameSession.cs
@@ -9,7 +9,8 @@ namespace AndrewStoddardGameOfPig.Models
     public class GameSession
     {
         private const string cpuScoreKey = "cpuscore";
-        private const string playerScoreKey = "playerscore";
+        private const string playerTotalScoreKey = "playertotalscore";
+        private const string playerRoundScoreKey = "playerroundscore";
         private ISession session { get; set; }
         public GameSession(ISession session)
         {
@@ -17,9 +18,12 @@ namespace AndrewStoddardGameOfPig.Models
         }
 
         public void SetCPUScore(int score) => this.session.SetInt32(cpuScoreKey, score);
-        public int? GetCPUScore => this.session.GetInt32(cpuScoreKey);
+        public int GetCPUScore => this.session.GetInt32(cpuScoreKey) ?? 0;
 
-        public void SetPlayerScore(int score) => this.session.SetInt32(playerScoreKey, score);
-        public int? GetPlayerScore => this.session.GetInt32(playerScoreKey);
+        public void SetPlayerTotalScore(int score) => this.session.SetInt32(playerTotalScoreKey, score);
+        public int GetPlayerTotalScore => this.session.GetInt32(playerTotalScoreKey) ?? 0;
+
+        public void SetPlayerRoundScore(int score) => this.session.SetInt32(playerRoundScoreKey, score);
+        public int GetPlayerRoundScore => this.session.GetInt32(playerRoundScoreKey) ?? 0;
     }
 }
