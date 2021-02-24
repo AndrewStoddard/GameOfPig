3fb0e90283c37dda269dc06cc1c7d041e3b0b0a5
AndrewStoddard <acs8182000@gmail.com>
2021-02-24 16:05:24 -0500

    Created a GameSession class to wrap around ISession

---
+++ AndrewStoddardGameOfPig/Models/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndrewStoddardGameOfPig.Models
+{
+    public class GameSession
+    {
+        private const string cpuScoreKey = "cpuscore";
+        private const string playerScoreKey = "playerscore";
+        private ISession session { get; set; }
+        public GameSession(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void SetCPUScore(int score) => this.session.SetInt32(cpuScoreKey, score);
+        public int? GetCPUScore => this.session.GetInt32(cpuScoreKey);
+
+        public void SetPlayerScore(int score) => this.session.SetInt32(playerScoreKey, score);
+        public int? GetPlayerScore => this.session.GetInt32(playerScoreKey);
+    }
+}
